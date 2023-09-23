f985649639c73259ac7b6be230d42ed76aa5eef7
Paul Njuga <paulnjuga@gmail.com>
2023-09-23 21:28:13 +0300

    Add func doc strings

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,11 +7,13 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+    '''Displays “Hello HBNB!”'''
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    '''Displays “HBNB”'''
     return "HBNB"
 
 
